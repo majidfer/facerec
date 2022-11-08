9d15f1ffa29922df555212756cea08cf47b8192f
feri <ferialfahri@gmail.com>
2022-11-08 10:50:29 +0000

    Styling the logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,11 @@
-import { React, useState } from "react";
+import React from "react";
 import Tilt from "react-parallax-tilt";
 import logo from "./face-id.png";
+import "./Logo.css";
 
 const Logo = () => {
   return (
-    <div className="ma4 mt0" style={{height: "150px", width: "150px"}}>
+    <div className="ma4 mt0 logo" style={{ height: "150px", width: "150px" }}>
       <Tilt tiltEnable={false} scale={1.3} transitionSpeed={2500}>
         <div className="b--dotted">
           <img src={logo} alt="logo" />
