078598af3c157a82ba203ecb0c6f09f104cefca6
feri <ferialfahri@gmail.com>
2022-11-09 11:58:17 +0000

    Adding Rank component + styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import ImageFormLink from './components/ImageFormLink/ImageFormLink';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg'
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg color="#81D99C" type="cobweb" bg={true} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageFormLink />
     </div>
   );
---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <>
+      <div className="f3 navy">{"Feri, your current rank is ..."}</div>
+      <div className="f1 navy">{"5"}</div>
+    </>
+  );
+};
+
+export default Rank;
