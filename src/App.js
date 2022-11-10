7b49a5c330e464f4db981628ebdba2b77f1613bd
feri <ferialfahri@gmail.com>
2022-11-10 23:31:07 +0000

    Adding clarifai app#

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-import ImageFormLink from './components/ImageFormLink/ImageFormLink';
-import Logo from './components/Logo/Logo';
-import Navigation from './components/Navigation/Navigation';
-import Rank from './components/Rank/Rank';
-import ParticlesBg from 'particles-bg'
+import "./App.css";
+import ImageFormLink from "./components/ImageFormLink/ImageFormLink";
+import Logo from "./components/Logo/Logo";
+import Navigation from "./components/Navigation/Navigation";
+import Rank from "./components/Rank/Rank";
+import ParticlesBg from "particles-bg";
 
 function App() {
   return (
--- src/components/ImageFormLink/ImageFormLink.jsx
+++ src/components/ImageFormLink/ImageFormLink.jsx
@@ -1,20 +1,44 @@
-import React from "react";
+import { React, useState } from "react";
+import Clarifai from "clarifai";
 import "./ImageFormLink.css";
 
 const ImageFormLink = () => {
+  const app = new Clarifai.App({
+    apiKey: "cfce0f69a25f4045ac3b0c6a9d82f9e4",
+  });
+
+  const [address, setAddress] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(app);
+    setAddress("");
+  };
+
   return (
     <div>
       <p className="f3">
         {"This app will detect faces in your pictures. Give it a try."}
       </p>
-      <div className="center">
-        <div className="pa4 br3 shadow-5 form center">
-          <input className="f4 pa2 w-70 center" type={"text"} />
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-green br2">
-            Detect
-          </button>
+      <form onSubmit={handleSubmit}>
+        <div className="center">
+          <div className="pa4 br3 shadow-5 form center">
+            <input
+              className="f4 pa2 w-70 center"
+              type={"text"}
+              name="address"
+              value={address}
+              onChange={(event) => setAddress(event.target.value)}
+            />
+            <button
+              className="w-30 grow f4 link ph3 pv2 dib white bg-green br2"
+              type="submit"
+            >
+              Detect
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
