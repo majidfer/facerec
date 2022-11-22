4ae8b572122568a11e8b0e661d5f787cecdd2fa9
feri <ferialfahri@gmail.com>
2022-11-22 19:22:03 +0000

    Updating string

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Rank = ({name, entries}) => {
   return (
     <>
-      <div className="f3 navy">{`${name}, your current rank is ...`}</div>
+      <div className="f3 navy">{`${name}, your current entry count is ...`}</div>
       <div className="f1 navy">{`${entries}`}</div>
     </>
   );
